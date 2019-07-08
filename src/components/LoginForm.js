fb248ddaf8acb7af38e80282a8a46a027a7534c3
Astrid88 <giu.munari@gmail.com>
2019-07-08 18:05:19 +0200

    add handle events

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
 export default function LoginForm (props){
-    
         return (
             <div>
                 <div id='loginForm'>
-                    <form>
-                        <input type='text' name='username' placeholder='Add a username'/>
-                        <input type='text' name='password' placeholder='Add a password'/>
-                        <button type='button'>Login</button>
+                    <form onSubmit={props.onSubmit}>
+                        <input type='text' name='username' placeholder='Add a username' onChange={props.onChange}/>
+                        <input type='text' name='password' placeholder='Add a password' onChange={props.onChange}/>
+                        <button type='submit' className='button'>Login</button>
                     </form>
                 </div>
             </div>
