c61f492aab6676cef82d37d4d6194b0eed34fa22
Astrid88 <giu.munari@gmail.com>
2019-07-11 12:41:39 +0200

    add css, next-game, points, check answer, check lifes

--- src/components/GameImage.js
+++ src/components/GameImage.js
@@ -2,14 +2,16 @@ import React from 'react'
 import './GameImage.css'
 
 export default function GameImage (props) {
-    const numberOfSquares = [...Array(12).keys()]
+    const numberOfSquares = [...Array(36).keys()]
         return (
             <div className='imageContainer'>
                 <div className='imageDiv'>
+                    {props.lifes!==undefined  &&
                         <ul className='imagePixelsContainer'>
                             {numberOfSquares.map(key=><li className='imagePixel' onClick={props.revealImage} key={key}></li>)}
                         </ul>
-                    <img src={props.urlImage} id='image' alt='kandinsky'/>
+                    }
+                    <img src={props.urlImage} id='image' alt='guess the painting'/>
                 </div>
                 
             </div>
