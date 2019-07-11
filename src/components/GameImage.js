52e96ceae803305c8fe4dc49d8450fe026119bfe
Astrid88 <giu.munari@gmail.com>
2019-07-11 10:12:19 +0200

    solve array of pixels

--- src/components/GameImage.js
+++ src/components/GameImage.js
@@ -2,22 +2,13 @@ import React from 'react'
 import './GameImage.css'
 
 export default function GameImage (props) {
+    const numberOfSquares = [...Array(12).keys()]
         return (
             <div className='imageContainer'>
                 <div className='imageDiv'>
-
                         <ul className='imagePixelsContainer'>
-                            <li className='imagePixel' onClick={props.revealImage}></li>
-                            <li className='imagePixel' onClick={props.revealImage}></li>  
-                            <li className='imagePixel' onClick={props.revealImage}></li>
-                            <li className='imagePixel' onClick={props.revealImage}></li>
-                            <li className='imagePixel' onClick={props.revealImage}></li>
-                            <li className='imagePixel' onClick={props.revealImage}></li>
-                            <li className='imagePixel' onClick={props.revealImage}></li>
-                            <li className='imagePixel' onClick={props.revealImage}></li>
-                            <li className='imagePixel' onClick={props.revealImage}></li>
+                            {numberOfSquares.map(key=><li className='imagePixel' onClick={props.revealImage} key={key}></li>)}
                         </ul>
-                        
                     <img src={props.urlImage} id='image' alt='kandinsky'/>
                 </div>
                 
