9eb9dff8b334e056b1c02f2e4ac006c4d9d80894
Alina <alinabeglarian@gmail.com>
2019-07-12 12:43:51 +0200

    minor redirect change

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import {withRouter} from 'react-router'
 import { connect } from 'react-redux'
 import ImageListContainer from './ImageListContainer';
@@ -7,8 +7,6 @@ import LoginFormContainer from './LoginFormContainer';
 import SignUpFormContainer from './SignUpFormContainer';
 import GameContainer from './GameContainer';
 import GameListContainer from './GameListContainer'
-import ScoreBoardContainer from './ScoreBoardContainer';
-import InstructionsContainer from './InstructionsContainer'
 import PlayGameButton from './PlayGameButton';
 
 function Routes(props) {
@@ -19,6 +17,8 @@ function Routes(props) {
         <div>
           <Route path="/" exact component={LoginFormContainer} />
           <Route path="/sign-up" exact component={SignUpFormContainer} />
+          <Route exact path="/gamelist" render={() => (<Redirect to="/" />)} />
+          <Route exact path="/game/:id" render={() => (<Redirect to="/" />)} />
         </div>
      }
 
@@ -27,13 +27,11 @@ function Routes(props) {
           <Route path="/" exact component={PlayGameButton} />
           <Route path="/game/:id" exact component={GameContainer} />
           <Route path="/gamelist" exact component={GameListContainer} />
-          <Route path='/scoreboard' exact component={ScoreBoardContainer} />
         </Switch>
       }
 
 
       <Route path="/" exact component={ImageListContainer} />
-      <Route path="/instructions" exact component={InstructionsContainer}/>
     </div>
   )
 }
